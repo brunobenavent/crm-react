8727973c60cd44f3e11b2a7e5b16893388b96c8a
Bruno Benavent Colomo <70907703+brunobenavent@users.noreply.github.com>
2022-10-05 13:47:00 +0200

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React, { Children } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
 import Layout from './components/Layout'
-import NuevoCliente from './pages/NuevoCliente'
-import Index from './pages/Index'
+import NuevoCliente, { action as nuevoClienteAction} from './pages/NuevoCliente'
+import Index, {loader as clientesLoader} from './pages/Index'
 
 
 const router = createBrowserRouter([
@@ -14,11 +14,13 @@ const router = createBrowserRouter([
     children: [
       {
         index:true,
-        element: <Index/>
+        element: <Index/>,
+        loader: clientesLoader
       },
       { 
         path: '/clientes/nuevo',
-        element : <NuevoCliente />
+        element : <NuevoCliente />,
+        action : nuevoClienteAction
       }
     ]
   }
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import Formulario from "../components/Formulario"
+import { useNavigate, Form } from "react-router-dom"
+export async function action({request}){
+  const formData = await request.formData()
+  const datos= Object.fromEntries(formData)
+  console.log(datos)
+}
+
 
 const NuevoCliente = () => {
+
+  const navigate = useNavigate()
   return (
-    <div>
-      <h1>nuevo cliente</h1>
-    </div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-3">Llena todos los campos para registrar un nuevo cliente</p>
+      <div className="flex justify-end">
+        <button
+          onClick={()=>navigate('/')}
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase">Volver</button>
+      </div>
+      <div className="bg-white shadow rounded-md mx-auto md:w-3/4 px-5 py-10">
+        <Form
+          method='post'
+
+        
+        >
+          <Formulario
+          
+          />
+          <input
+            type='submit'
+            className="w-full mt-5 bg-blue-800 hover:bg-blue-700 hover:cursor-pointer p-3 uppercase font-bold text-white text-lg"
+            value = 'Registrar Cliente'
+          />
+        </Form>
+      </div>
+    </>
   )
 }
 
